97c0329d86a99a95c90298ced359db6eae43e914
SalvadorLimones <87721843+SalvadorLimones@users.noreply.github.com>
2022-05-25 10:00:20 +0200

    add userschema with nested dashboard and todo

---
+++ backend/model/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+  title: { type: String, required: true }, //empty string NONO!
+  content: { type: String, required: true }, //empty string is enough
+  isDone: { type: Boolean, default: false },
+});
+
+const dashboardSchema = new mongoose.Schema({
+  title: { type: String, required: true }, //empty string NONO!
+  todos: [todoSchema], //empty list as default?
+});
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true, required: true }, //empty string NONO!
+  email: { type: String, unique: true, required: true }, //empty string NONO! + validation
+  password: { type: String, required: true }, //empty string NONO! + validation
+  dashboards: [dashboardSchema], //empty list as default?
+});
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
