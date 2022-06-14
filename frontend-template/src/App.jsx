e4f6a789a5bcea9f87027ac44819729d73985599
agolautner <lautner.agoston@gmail.com>
2022-06-14 10:05:24 +0200

    add react routing

--- frontend-template/src/App.jsx
+++ frontend-template/src/App.jsx
@@ -1,18 +1,19 @@
 import './App.css';
-import NumberModifier from './NumberModifier';
-import { useCounter } from './CounterProvider';
-import NumberPresenter from './NumberPresenter';
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
 function App() {
-  const {value, increment, decrement} = useCounter();
   return (
       <div className="App">
-        <h2>Counter</h2>
-        <button onClick={increment}>+</button>
-        <p>{value}</p>
-        <button onClick={decrement}>-</button>
-        <NumberPresenter />
-        <NumberModifier/>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+        </Routes>
       </div>
   );
 }
---
+++ frontend-template/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useNavigate } from "react-router-dom";
+
+const Navbar = () => {
+    const navigate = useNavigate();
+    const nav = (path) => {
+        console.log('rerouting'); //any logic we want to run before rerouting
+        navigate(path);
+    }
+    return (
+        <nav>
+            {/* we can call the navigate function directly */}
+            <button onClick={() => navigate('/')}>HOME</button> 
+            <button onClick={() => navigate('/about')}>ABOUT</button>
+            {/* or implement a function which allows for logic before rerouting */}
+            <button onClick={() => nav('/profile')}>PROFILE</button>
+        </nav>
+  )
+}
+
+export default Navbar
