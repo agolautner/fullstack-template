385ffa7fe6170d9249381e299c14fc5378f6b12f
agolautner <lautner.agoston@gmail.com>
2022-06-21 15:10:02 +0200

    different logins can now be connected

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -7,7 +7,7 @@ import { useAuth } from '../providers/auth';
 const Register = () => {
     const [username, setUsername] = useState('');
     const navigate = useNavigate();
-    const { register, user } = useAuth();
+    const { register, user, auth } = useAuth();
 
     useEffect(() => {
         console.log(user);
@@ -19,6 +19,11 @@ const Register = () => {
         <div>Register</div>
         <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder='choose a username'/>
         <button onClick={() => register(username)}>register</button>
+        <hr />
+        <h1>OR!</h1>
+        <hr />
+        <button onClick={() => auth('google')}>google</button>
+        <button onClick={() => auth('oid')}>oid</button>
     </>
   )
 }
